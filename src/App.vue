6610f51e0cc433e4f0d35c68e9f9e108e0c49e62
Allen_glb <goodMorning_glb@rongda.com>
2020-02-21 21:42:29 +0800

    Vue__vuex_counter应用(vue版本)

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,56 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <p>click: {{count}} times, count is {{evenOrOdd}}</p>
+    <button @click="increment">+</button>
+    <button @click="decrement">-</button>
+    <button @click="incrementIfOdd">increment if odd</button>
+    <button @click="incrementAsync">increment async</button>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data(){
+    return {
+      count: 0
+    }
+  },
+  computed: {
+    evenOrOdd() {
+      return this.count % 2 === 0 ? '偶数' : '奇数';
+    }
+  },
+  methods: {
+    //增加
+    increment() {
+      let count = this.count;
+      this.count = ++count;
+    },
+    //减少
+    decrement() {
+      let count = this.count;
+      this.count = --count;
+    },
+    //如果是奇数才增加
+    incrementIfOdd() {
+      let count = this.count;
+      if(count % 2 === 1){
+        this.count = count + 1;
+      }
+    },
+    //过1秒才增加
+    incrementAsync() {
+      setTimeout(() => {
+        let count = this.count;
+        this.count = count + 1;
+      }, 1000)
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
