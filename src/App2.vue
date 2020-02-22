be84477a81be400525ccbb126af83235d0b1cc14
Allen_glb <goodMorning_glb@rongda.com>
2020-02-22 17:26:46 +0800

    备份：Vue__vuex_counter应用(vuex版本1) 修改的App.vue，store.js

---
+++ src/App2.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+    <p>click: {{$store.state.count}} times, count is {{evenOrOdd}}</p>
+    <button @click="increment">+</button>
+    <button @click="decrement">-</button>
+    <button @click="incrementIfOdd">increment if odd</button>
+    <button @click="incrementAsync">increment async</button>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  computed: {
+    evenOrOdd() {
+      return this.$store.getters.evenOrOdd;
+    }
+  },
+  methods: {
+    //增加
+    increment() {
+      //通知vuex去增加
+      this.$store.dispatch('increment') //触发store中对应的action调用
+    },
+    //减少
+    decrement() {
+      //通知vuex去减少
+      this.$store.dispatch('decrement')
+    },
+    //如果是奇数才增加
+    incrementIfOdd() {
+      this.$store.dispatch('incrementIfOdd')
+    },
+    //过1秒才增加
+    incrementAsync() {
+      this.$store.dispatch('incrementAsync')
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
