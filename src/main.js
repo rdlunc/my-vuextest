478edefa7e4e0df17434070490c8c47c089642c2
Allen_glb <goodMorning_glb@rongda.com>
2020-02-22 20:17:35 +0800

    创建 Vue__vuex_todolist应用（vue版）

---
+++ src/main.js
@@ -0,0 +1,22 @@
+/*
+入口JS
+ */
+import Vue from 'vue'
+import App from './App.vue'
+
+import './base.css'
+import store from "../src-counter/store";
+
+// 创建vm
+//compiler
+// new Vue({
+//   el: '#app',
+//   components: {App}, // 映射组件标签
+//   template: '<App/>' // 指定需要渲染到页面的模板
+// })
+
+//runtime
+new Vue({
+  render: h => h(App),
+  store //所有组件对象都多一个属性：$store
+}).$mount('#app')
